3e7e2f9ac71b3a71eaffcc2d0145c648845690f8
John Petrov <johnnycontactmail@gmail.com>
2025-05-16 23:05:06 +0200

    changes to frontend blank page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import './styles/index.css';
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
